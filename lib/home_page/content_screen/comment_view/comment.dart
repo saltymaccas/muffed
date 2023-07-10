639f755d7aa254d8b00c0601ff5fbee60dc03fae
freshfieldreds <freshfieldreds@gmail.com>
2023-07-11 08:43:49 +1000

    Styled comments and added indenting for the replies

---
+++ lib/home_page/content_screen/comment_view/comment.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:muffed/repo/lemmy/models.dart';
+
+class CommentItem extends StatelessWidget {
+  const CommentItem(this.comment, {super.key});
+
+  final LemmyComment comment;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          if (comment.level != 0) ...[
+            Container(
+              width: 1,
+              color: Theme.of(context).colorScheme.outline,
+            ),
+          ],
+          SizedBox(
+            width: 5,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      comment.creatorName,
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary),
+                    )
+                  ],
+                ),
+                Text(comment.content),
+                for (final LemmyComment reply in comment.replies)
+                  CommentItem(reply)
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home_page/content_screen/content_screen.dart
+++ lib/home_page/content_screen/content_screen.dart
@@ -5,6 +5,7 @@ import 'package:muffed/components/loading.dart';
 import 'package:muffed/repo/server_repo.dart';
 import 'package:muffed/home_page/post_view/card.dart';
 import 'bloc/bloc.dart';
+import 'comment_view/comment.dart';
 
 class ContentScreen extends StatelessWidget {
   const ContentScreen(this.post, {super.key});
@@ -48,7 +49,7 @@ class ContentScreen extends StatelessWidget {
                               delegate: SliverChildBuilderDelegate(
                                   childCount: state.comments!.length,
                                   (context, index) {
-                                return Text(state.comments![index].content);
+                                return CommentItem(state.comments![index]);
                               }),
                             ),
             ],
