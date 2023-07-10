a1f963e0dfb155749a6e2c835430647c1214453b
freshfieldreds <freshfieldreds@gmail.com>
2023-07-11 09:46:23 +1000

    added isLoadingMore value

--- lib/home_page/content_screen/bloc/state.dart
+++ lib/home_page/content_screen/bloc/state.dart
@@ -6,20 +6,23 @@ class ContentScreenState extends Equatable{
   final ContentScreenStatus status;
   final List<LemmyComment>? comments;
   final int pagesLoaded;
+  final bool isLoadingMore;
 
 
-  ContentScreenState({required this.status, this.comments, this.pagesLoaded = 0});
+  ContentScreenState({required this.status, this.comments, this.pagesLoaded = 0, this.isLoadingMore = false});
 
 
   @override
-  List<Object?> get props => [status, comments, pagesLoaded];
+  List<Object?> get props => [status, comments, pagesLoaded, isLoadingMore];
 
   ContentScreenState copyWith({
+    bool? isLoadingMore,
     ContentScreenStatus? status,
     List<LemmyComment>? comments,
     int? pagesLoaded,
   }) {
     return ContentScreenState(
+      isLoadingMore: isLoadingMore ?? this.isLoadingMore,
       status: status ?? this.status,
       comments: comments ?? this.comments,
       pagesLoaded: pagesLoaded ?? this.pagesLoaded,
