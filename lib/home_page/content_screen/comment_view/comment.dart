650f384c959f927ab947924d58876b5d0f931fd8
freshfieldreds <freshfieldreds@gmail.com>
2023-07-11 12:19:49 +1000

    added action buttons to comment

--- lib/home_page/content_screen/comment_view/comment.dart
+++ lib/home_page/content_screen/comment_view/comment.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:muffed/repo/lemmy/models.dart';
 
-class CommentItem extends StatelessWidget {
+class CommentItem extends StatefulWidget {
   const CommentItem(this.comment, {super.key});
 
   final LemmyComment comment;
+
+  @override
+  State<CommentItem> createState() => _CommentItemState();
+}
+
+class _CommentItemState extends State<CommentItem> {
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +18,7 @@ class CommentItem extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          if (comment.level != 0) ...[
+          if (widget.comment.level != 0) ...[
             Container(
               width: 1,
               color: Theme.of(context).colorScheme.outline,
@@ -28,14 +34,42 @@ class CommentItem extends StatelessWidget {
                 Row(
                   children: [
                     Text(
-                      comment.creatorName,
+                      widget.comment.creatorName,
                       style: TextStyle(
                           color: Theme.of(context).colorScheme.primary),
                     )
                   ],
                 ),
-                Text(comment.content),
-                for (final LemmyComment reply in comment.replies)
+                Text(widget.comment.content),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_upward_rounded),
+                      visualDensity: VisualDensity.compact,
+                    ),
+                    Text(widget.comment.upVotes.toString()),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_downward_rounded),
+                      visualDensity: VisualDensity.compact,
+                    ),
+                    Text(widget.comment.downVotes.toString()),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.reply),
+                      visualDensity: VisualDensity.compact,
+                    ),
+                    IconButton(
+                        onPressed: () {}, icon: const Icon(Icons.more_vert)),
+                  ],
+                ),
+                for (final LemmyComment reply in widget.comment.replies)
                   CommentItem(reply)
               ],
             ),
