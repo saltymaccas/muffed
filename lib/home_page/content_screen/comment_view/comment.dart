d8bf6afdd488e0ae951111fa3fb7d232829ed237
freshfieldreds <freshfieldreds@gmail.com>
2023-07-11 14:56:17 +1000

    added time ago posted to comment

--- lib/home_page/content_screen/comment_view/comment.dart
+++ lib/home_page/content_screen/comment_view/comment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:muffed/repo/lemmy/models.dart';
+import 'package:muffed/utils/utils.dart';
 
 class CommentItem extends StatefulWidget {
   const CommentItem(this.comment, {super.key});
@@ -11,19 +12,18 @@ class CommentItem extends StatefulWidget {
 }
 
 class _CommentItemState extends State<CommentItem> {
-
   bool isMinimised = false;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
+      onTap: () {
         setState(() {
           isMinimised = !isMinimised;
         });
       },
       child: IntrinsicHeight(
-        child:Row(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             if (widget.comment.level != 0) ...[
@@ -36,52 +36,68 @@ class _CommentItemState extends State<CommentItem> {
               width: 5,
             ),
             Expanded(
-              child: (!isMinimised) ? Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        widget.comment.creatorName,
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary),
+                child: (!isMinimised)
+                    ? Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                widget.comment.creatorName,
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary),
+                              ),
+                              const SizedBox(width: 10,),
+                              Text(
+                                formattedPostedAgo(
+                                  widget.comment.timePublished,
+                                ),
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.outline),
+                              ),
+                            ],
+                          ),
+                          Text(widget.comment.content),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.arrow_upward_rounded),
+                                visualDensity: VisualDensity.compact,
+                              ),
+                              Text(widget.comment.upVotes.toString()),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.arrow_downward_rounded),
+                                visualDensity: VisualDensity.compact,
+                              ),
+                              Text(widget.comment.downVotes.toString()),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.reply),
+                                visualDensity: VisualDensity.compact,
+                              ),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.more_vert)),
+                            ],
+                          ),
+                          for (final LemmyComment reply
+                              in widget.comment.replies)
+                            CommentItem(reply)
+                        ],
                       )
-                    ],
-                  ),
-                  Text(widget.comment.content),
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.arrow_upward_rounded),
-                        visualDensity: VisualDensity.compact,
-                      ),
-                      Text(widget.comment.upVotes.toString()),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.arrow_downward_rounded),
-                        visualDensity: VisualDensity.compact,
-                      ),
-                      Text(widget.comment.downVotes.toString()),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.reply),
-                        visualDensity: VisualDensity.compact,
-                      ),
-                      IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.more_vert)),
-                    ],
-                  ),
-                  for (final LemmyComment reply in widget.comment.replies)
-                    CommentItem(reply)
-                ],
-              ) : Text(widget.comment.content, maxLines: 1,)
-            ),
+                    : Text(
+                        widget.comment.content,
+                        maxLines: 1,
+                      )),
           ],
         ),
       ),
