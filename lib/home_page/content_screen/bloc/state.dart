9d1cdf9c95df01d2706bbd56c7802dba0a610ec4
freshfieldreds <freshfieldreds@gmail.com>
2023-07-11 10:34:00 +1000

    added reached end value

--- lib/home_page/content_screen/bloc/state.dart
+++ lib/home_page/content_screen/bloc/state.dart
@@ -7,21 +7,24 @@ class ContentScreenState extends Equatable{
   final List<LemmyComment>? comments;
   final int pagesLoaded;
   final bool isLoadingMore;
+  final bool reachedEnd;
 
 
-  ContentScreenState({required this.status, this.comments, this.pagesLoaded = 0, this.isLoadingMore = false});
+  ContentScreenState({required this.status, this.comments, this.pagesLoaded = 0, this.isLoadingMore = false, this.reachedEnd = false});
 
 
   @override
-  List<Object?> get props => [status, comments, pagesLoaded, isLoadingMore];
+  List<Object?> get props => [status, comments, pagesLoaded, isLoadingMore, reachedEnd];
 
   ContentScreenState copyWith({
+    bool? reachedEnd,
     bool? isLoadingMore,
     ContentScreenStatus? status,
     List<LemmyComment>? comments,
     int? pagesLoaded,
   }) {
     return ContentScreenState(
+      reachedEnd: reachedEnd ?? this.reachedEnd,
       isLoadingMore: isLoadingMore ?? this.isLoadingMore,
       status: status ?? this.status,
       comments: comments ?? this.comments,
