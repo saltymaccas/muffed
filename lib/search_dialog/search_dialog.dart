62a7207b9feca1341c91f408489b570e5314b30c
freshfieldreds <freshfieldreds@gmail.com>
2023-07-20 19:14:21 +1000

    added animated size

--- lib/search_dialog/search_dialog.dart
+++ lib/search_dialog/search_dialog.dart
@@ -18,106 +18,110 @@ void openSearchDialog(BuildContext context) {
             insetPadding: EdgeInsets.fromLTRB(8, 8, 8, 8),
             child: BlocBuilder<SearchBloc, SearchState>(
               builder: (context, state) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(
-                      child: ListView.builder(
-                          reverse: true,
-                          clipBehavior: Clip.hardEdge,
-                          itemCount: state.communities.length,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                if (state.communities.length - 1 == index)
-                                  SizedBox(
-                                    height: 8,
-                                  ),
-                                InkWell(
-                                  onTap: () {
-                                    context.pop();
-                                    context.go(Uri(
-                                        path: '/home/community',
-                                        queryParameters: {
-                                          'id': state.communities[index].id
-                                              .toString()
-                                        }).toString());
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8),
-                                    child: Row(
-                                      children: [
-                                        CircleAvatar(
-                                          radius: 12,
-                                          child: ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(45),
-                                            child: (state.communities[index]
-                                                        .icon !=
-                                                    null)
-                                                ? Image.network(state
-                                                        .communities[index]
-                                                        .icon! +
-                                                    '?thumbnail=50')
-                                                : SvgPicture.asset(
-                                                    'assets/logo.svg'),
+                return AnimatedSize(
+                  duration: Duration(milliseconds: 500),
+                  curve: Curves.easeInOutCubic,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                        child: ListView.builder(
+                            reverse: true,
+                            clipBehavior: Clip.hardEdge,
+                            itemCount: state.communities.length,
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  if (state.communities.length - 1 == index)
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                  InkWell(
+                                    onTap: () {
+                                      context.pop();
+                                      context.go(Uri(
+                                          path: '/home/community',
+                                          queryParameters: {
+                                            'id': state.communities[index].id
+                                                .toString()
+                                          }).toString());
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 8),
+                                      child: Row(
+                                        children: [
+                                          CircleAvatar(
+                                            radius: 12,
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(45),
+                                              child: (state.communities[index]
+                                                          .icon !=
+                                                      null)
+                                                  ? Image.network(state
+                                                          .communities[index]
+                                                          .icon! +
+                                                      '?thumbnail=50')
+                                                  : SvgPicture.asset(
+                                                      'assets/logo.svg'),
+                                            ),
                                           ),
-                                        ),
-                                        SizedBox(
-                                          width: 8,
-                                        ),
-                                        Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(state.communities[index].name),
-                                            Text(
-                                              '${state.communities[index].subscribers} subscribers',
-                                              style: TextStyle(
-                                                fontSize: 10,
-                                                color: Theme.of(context)
-                                                    .colorScheme
-                                                    .outline,
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      ],
+                                          SizedBox(
+                                            width: 8,
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(state.communities[index].name),
+                                              Text(
+                                                '${state.communities[index].subscribers} subscribers',
+                                                style: TextStyle(
+                                                  fontSize: 10,
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .outline,
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Divider(),
-                              ],
-                            );
-                          }),
-                    ),
-                    TextField(
-                      onChanged: (query) {
-                        context
-                            .read<SearchBloc>()
-                            .add(SearchQueryChanged(query));
-                      },
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.search),
-                        ),
-                        prefixIcon: IconButton(
-                            visualDensity: VisualDensity.compact,
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: Icon(Icons.arrow_back)),
-                        hintText: 'Search',
-                        focusedBorder: InputBorder.none,
-                        border: InputBorder.none,
+                                  Divider(),
+                                ],
+                              );
+                            }),
                       ),
-                    ),
-                  ],
+                      TextField(
+                        onChanged: (query) {
+                          context
+                              .read<SearchBloc>()
+                              .add(SearchQueryChanged(query));
+                        },
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.search),
+                          ),
+                          prefixIcon: IconButton(
+                              visualDensity: VisualDensity.compact,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(Icons.arrow_back)),
+                          hintText: 'Search',
+                          focusedBorder: InputBorder.none,
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
