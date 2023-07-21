2fccf773e52d85137b285cc1c703913665d8c866
freshfieldreds <freshfieldreds@gmail.com>
2023-07-21 18:16:03 +1000

    started assembling the profile page

--- lib/profile_page/profile_page.dart
+++ lib/profile_page/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:muffed/dynamic_navigation_bar/dynamic_navigation_bar.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -9,8 +10,21 @@ class ProfilePage extends StatelessWidget {
     return SetPageInfo(
         itemIndex: 2,
         actions: [],
-        child: const Placeholder(
-          color: Colors.blue,
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.account_circle,
+                size: 100,
+              ),
+              TextButton(onPressed: () {
+                showModalBottomSheet(context: context, builder: (context) {
+                  return Container();
+                });
+              }, child: Text('Anonymous'))
+            ],
+          ),
         ));
   }
 }
