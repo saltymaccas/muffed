712c900b44c0e361a1dde0893150b867fca9586e
freshfieldreds <freshfieldreds@gmail.com>
2023-07-21 10:15:15 +1000

    removed unneccarary import

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:muffed/profile_page/profile_page.dart';
 import 'package:muffed/content_screen/content_screen.dart';
 import 'package:muffed/repo/server_repo.dart';
 import 'dynamic_navigation_bar/dynamic_navigation_bar.dart';
-import 'dynamic_navigation_bar/bloc/bloc.dart';
 import 'package:muffed/community_screen/community_screen.dart';
 
 final _router = GoRouter(
