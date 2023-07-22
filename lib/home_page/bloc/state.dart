2211b70e0f80e1ff0182b619b16cdd658d95d4a9
freshfieldreds <freshfieldreds@gmail.com>
2023-07-22 10:02:11 +1000

    added error message parameter

--- lib/home_page/bloc/state.dart
+++ lib/home_page/bloc/state.dart
@@ -8,13 +8,23 @@ final class HomePageState extends Equatable {
   final bool isRefreshing;
   final int pagesLoaded;
   final bool isLoadingMore;
+  final String? errorMessage;
 
-  HomePageState({required this.status, this.posts, this.isRefreshing = false, this.pagesLoaded = 1, this.isLoadingMore = false});
+  HomePageState({
+    required this.status,
+    this.posts,
+    this.isRefreshing = false,
+    this.pagesLoaded = 1,
+    this.isLoadingMore = false,
+    this.errorMessage,
+  });
 
   @override
-  List<Object?> get props => [posts, status, isRefreshing, pagesLoaded, isLoadingMore];
+  List<Object?> get props =>
+      [posts, status, isRefreshing, pagesLoaded, isLoadingMore, errorMessage];
 
   HomePageState copyWith({
+    String? errorMessage,
     HomePageStatus? status,
     List? posts,
     bool? isRefreshing,
@@ -22,6 +32,7 @@ final class HomePageState extends Equatable {
     bool? isLoadingMore,
   }) {
     return HomePageState(
+      errorMessage: errorMessage,
       status: status ?? this.status,
       posts: posts ?? this.posts,
       isRefreshing: isRefreshing ?? this.isRefreshing,
