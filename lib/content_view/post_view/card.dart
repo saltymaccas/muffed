cff6bebb20b3db6ac5dfe8fb381f0b6e454cfa9b
freshfieldreds <freshfieldreds@gmail.com>
2023-07-23 19:06:40 +1000

    add ability to upvote and downvote

--- lib/content_view/post_view/card.dart
+++ lib/content_view/post_view/card.dart
@@ -1,31 +1,49 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:muffed/repo/server_repo.dart';
 import 'package:muffed/utils/utils.dart';
-import 'post_more_actions_sheet/post_more_actions_sheet.dart';
+import 'post_more_actions_sheet.dart';
 import 'package:any_link_preview/any_link_preview.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class CardLemmyPostItem extends StatelessWidget {
+class CardLemmyPostItem extends StatefulWidget {
   final LemmyPost post;
   final bool limitContentHeight;
   final void Function(dynamic post)? openContent;
+  final LemmyVoteType voteType;
 
   const CardLemmyPostItem(this.post,
-      {this.openContent, this.limitContentHeight = true, super.key});
+      {this.openContent,
+      this.limitContentHeight = true,
+      this.voteType = LemmyVoteType.none,
+      super.key});
+
+  @override
+  State<CardLemmyPostItem> createState() => _CardLemmyPostItemState();
+}
+
+class _CardLemmyPostItemState extends State<CardLemmyPostItem> {
+  LemmyVoteType voteType = LemmyVoteType.none;
+
+  @override
+  void initState() {
+    voteType = widget.voteType;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: InkWell(
         onTap: () {
-          if (openContent != null) {
-            openContent!(post);
+          if (widget.openContent != null) {
+            widget.openContent!(widget.post);
           }
         },
         child: Column(
@@ -38,7 +56,8 @@ class CardLemmyPostItem extends StatelessWidget {
                 children: [
                   GestureDetector(
                     onTap: () {
-                      context.push('/home/community?id=${post.communityId}');
+                      context.push(
+                          '/home/community?id=${widget.post.communityId}');
                     },
                     child: Row(
                       children: [
@@ -48,15 +67,15 @@ class CardLemmyPostItem extends StatelessWidget {
                               radius: 12,
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(45),
-                                child: (post.communityIcon != null)
-                                    ? Image.network(
-                                        post.communityIcon! + '?thumbnail=50')
+                                child: (widget.post.communityIcon != null)
+                                    ? Image.network(widget.post.communityIcon! +
+                                        '?thumbnail=50')
                                     : SvgPicture.asset('assets/logo.svg'),
                               ),
                             ),
                             const VerticalDivider(),
                             Text(
-                              post.communityName,
+                              widget.post.communityName,
                               style: TextStyle(
                                 color: Theme.of(context).colorScheme.primary,
                               ),
@@ -65,14 +84,15 @@ class CardLemmyPostItem extends StatelessWidget {
                         ),
                         const VerticalDivider(),
                         Text(
-                          post.creatorName,
+                          widget.post.creatorName,
                           style: TextStyle(
                             color: Theme.of(context).colorScheme.outline,
                           ),
                         ),
                         const VerticalDivider(),
                         Text(
-                          formattedPostedAgo(post.timePublished) + ' ago',
+                          formattedPostedAgo(widget.post.timePublished) +
+                              ' ago',
                           style: TextStyle(
                               color: Theme.of(context).colorScheme.outline),
                         ),
@@ -82,25 +102,25 @@ class CardLemmyPostItem extends StatelessWidget {
                   const SizedBox(
                     height: 5,
                   ),
-                  Text(post.name, style: TextStyle(fontSize: 16)),
+                  Text(widget.post.name, style: TextStyle(fontSize: 16)),
                 ],
               ),
             ),
             Builder(builder: (context) {
               return Column(
                 children: [
-                  if (post.url != null) ...[
+                  if (widget.post.url != null) ...[
                     Builder(builder: (context) {
-                      if (post.url!.contains('.jpg') ||
-                          post.url!.contains('.png') ||
-                          post.url!.contains('.jpeg') ||
-                          post.url!.contains('.gif') ||
-                          post.url!.contains('.webp') ||
-                          post.url!.contains('.bmp')) {
+                      if (widget.post.url!.contains('.jpg') ||
+                          widget.post.url!.contains('.png') ||
+                          widget.post.url!.contains('.jpeg') ||
+                          widget.post.url!.contains('.gif') ||
+                          widget.post.url!.contains('.webp') ||
+                          widget.post.url!.contains('.bmp')) {
                         return SizedBox(
                             height: 300,
                             child: Center(
-                              child: Image.network(post.url!,
+                              child: Image.network(widget.post.url!,
                                   fit: BoxFit.fitWidth),
                             ));
                       } else {
@@ -111,15 +131,16 @@ class CardLemmyPostItem extends StatelessWidget {
                             child: AnyLinkPreview(
                               errorImage: 'null',
                               errorBody: 'Could not load body',
-                              errorTitle: post.name,
+                              errorTitle: widget.post.name,
                               errorWidget: GestureDetector(
-                                onTap: () => launchUrl(Uri.parse(post.url!)),
+                                onTap: () =>
+                                    launchUrl(Uri.parse(widget.post.url!)),
                                 child: Container(
                                   color:
                                       Theme.of(context).colorScheme.background,
                                   padding: EdgeInsets.all(4),
                                   child: Text(
-                                    post.url!,
+                                    widget.post.url!,
                                     style: const TextStyle(
                                         decoration: TextDecoration.underline),
                                   ),
@@ -129,7 +150,7 @@ class CardLemmyPostItem extends StatelessWidget {
                               removeElevation: true,
                               borderRadius: 10,
                               boxShadow: [],
-                              link: post.url!,
+                              link: widget.post.url!,
                               backgroundColor:
                                   Theme.of(context).colorScheme.background,
                               displayDirection:
@@ -142,7 +163,7 @@ class CardLemmyPostItem extends StatelessWidget {
                       }
                     })
                   ],
-                  if (post.body != '' && post.body != null) ...[
+                  if (widget.post.body != '' && widget.post.body != null) ...[
                     Padding(
                       padding: EdgeInsets.all(4),
                       child: Container(
@@ -155,13 +176,13 @@ class CardLemmyPostItem extends StatelessWidget {
                         ),
                         child: Padding(
                           padding: const EdgeInsets.all(4),
-                          child: (limitContentHeight)
+                          child: (widget.limitContentHeight)
                               ? Text(
-                                  post.body!,
+                                  widget.post.body!,
                                   maxLines: 10,
                                 )
                               : MarkdownBody(
-                                  data: post.body!,
+                                  data: widget.post.body!,
                                   shrinkWrap: true,
                                   onTapLink: (text, link, title) {
                                     launchUrl(Uri.parse(link!));
@@ -186,27 +207,53 @@ class CardLemmyPostItem extends StatelessWidget {
                       const SizedBox(
                         width: 5,
                       ),
-                      Text('${post.commentCount}'),
+                      Text('${widget.post.commentCount}'),
                     ],
                   ),
                   Row(
                     children: [
                       IconButton(
                         icon: const Icon(Icons.arrow_upward_outlined),
-                        onPressed: () {},
+                        color: (voteType == LemmyVoteType.upVote)
+                            ? Colors.deepOrange
+                            : null,
+                        onPressed: () async {
+                          try {
+                            await context.read<ServerRepo>().lemmyRepo.votePost(
+                              widget.post.id,
+                              LemmyVoteType.upVote,
+                            );
+                            setState(() {
+                              voteType = LemmyVoteType.upVote;
+                            });
+                          } catch (err) {}
+                        },
                         visualDensity: VisualDensity.compact,
                       ),
-                      Text(post.upVotes.toString()),
+                      Text(widget.post.upVotes.toString()),
                       IconButton(
                         icon: const Icon(Icons.arrow_downward_outlined),
-                        onPressed: () {},
+                        color: (voteType == LemmyVoteType.downVote)
+                            ? Colors.purple
+                            : null,
+                        onPressed: () async {
+                          try {
+                            await context.read<ServerRepo>().lemmyRepo.votePost(
+                                  widget.post.id,
+                                  LemmyVoteType.downVote,
+                                );
+                            setState(() {
+                              voteType = LemmyVoteType.downVote;
+                            });
+                          } catch (err) {}
+                        },
                         visualDensity: VisualDensity.compact,
                       ),
-                      Text(post.downVotes.toString()),
+                      Text(widget.post.downVotes.toString()),
                       IconButton(
                         icon: const Icon(Icons.more_vert),
                         onPressed: () {
-                          showPostMoreActionsSheet(context, post);
+                          showPostMoreActionsSheet(context, widget.post);
                         },
                         visualDensity: VisualDensity.compact,
                       ),
