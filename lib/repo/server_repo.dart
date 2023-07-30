ae4f7dad47175e3f328b7e56663dacc440f7506d
freshfieldreds <freshfieldreds@gmail.com>
2023-07-30 18:11:14 +1000

    removed unnecessary code

--- lib/repo/server_repo.dart
+++ lib/repo/server_repo.dart
@@ -1,7 +1,5 @@
-import '../global_state/bloc.dart';
+import 'package:muffed/global_state/bloc.dart';
 import 'lemmy.dart';
-import 'lemmy/models.dart';
-import 'package:dio/dio.dart';
 
 export 'lemmy/models.dart';
 
@@ -10,11 +8,6 @@ interface class ServerRepo {
   final GlobalBloc globalBloc;
 
   ServerRepo(this.globalBloc) : lemmyRepo = LemmyRepo(globalBloc: globalBloc);
-
-  Future<List> getPosts(
-      {LemmySortType sortType = LemmySortType.hot, int page = 1}) async {
-    return await lemmyRepo.getPosts(sortType: sortType, page: page);
-  }
 }
 
 
