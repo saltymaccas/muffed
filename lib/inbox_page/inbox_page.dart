befa5d9142d781978642b380ef07e6b532210e75
freshfieldreds <freshfieldreds@gmail.com>
2023-09-16 18:54:55 +1000

    add coming soon text to inbox page

--- lib/inbox_page/inbox_page.dart
+++ lib/inbox_page/inbox_page.dart
@@ -6,6 +6,11 @@ class InboxPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SetPageInfo(actions: [], indexOfRelevantItem: 1,child: const Placeholder());
+    return SetPageInfo(
+        actions: [],
+        indexOfRelevantItem: 1,
+        child: const Placeholder(
+          child: Center(child: Text('Coming soon')),
+        ));
   }
 }
