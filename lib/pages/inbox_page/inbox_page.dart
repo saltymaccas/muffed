994686ad9d5791d054aa715f5687327c137f4691
freshfieldreds <freshfieldreds@gmail.com>
2023-10-28 21:11:17 +1100

    add differnt button to bottom nav bar that shows and hides all

--- lib/pages/inbox_page/inbox_page.dart
+++ lib/pages/inbox_page/inbox_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:muffed/dynamic_navigation_bar/bloc/bloc.dart';
+import 'package:muffed/dynamic_navigation_bar/dynamic_navigation_bar.dart';
 import 'package:muffed/pages/inbox_page/replies_screen/replies_screen.dart';
 import 'package:muffed/repo/server_repo.dart';
 
-import '../../components/popup_menu/popup_menu.dart';
 import 'mentions_screen/bloc/bloc.dart' as mentions;
 import 'mentions_screen/mentions_screen.dart';
 import 'replies_screen/bloc/bloc.dart' as replies;
@@ -29,111 +28,58 @@ class InboxPage extends StatelessWidget {
       ],
       child: Builder(
         builder: (context) {
-          context.read<DynamicNavigationBarBloc>().add(
-                EditPageActions(
-                  context: context,
-                  itemIndex: 1,
-                  actions: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.more_vert),
-                      visualDensity: VisualDensity.compact,
-                    ),
-                  ],
-                ),
-              );
-
           return DefaultTabController(
             length: 2,
             child: Builder(
               builder: (context) {
                 final blocContext = context;
 
-                final tabController = DefaultTabController.of(context);
+                void toggleRead() {
+                  context
+                      .read<mentions.MentionsBloc>()
+                      .add(mentions.ShowAllToggled());
+                  context
+                      .read<replies.RepliesBloc>()
+                      .add(replies.ShowAllToggled());
+                }
 
-                tabController.animation!.addListener(() {
-                  context.read<DynamicNavigationBarBloc>().add(
-                        EditPageActions(
-                          context: context,
-                          itemIndex: 1,
-                          actions: [],
-                        ),
-                      );
-                });
-
-                tabController.addListener(() {
-                  if (tabController.index == 0) {
-                    context.read<DynamicNavigationBarBloc>().add(
-                          EditPageActions(
-                            context: context,
-                            itemIndex: 1,
-                            actions: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.more_vert),
-                                visualDensity: VisualDensity.compact,
-                              ),
-                            ],
-                          ),
-                        );
-                  } else if (tabController.index == 1) {
-                    context.read<DynamicNavigationBarBloc>().add(
-                          EditPageActions(
-                            context: context,
-                            itemIndex: 1,
-                            actions: [
-                              MuffedPopupMenuButton(
-                                icon: Icon(Icons.more_vert),
-                                visualDensity: VisualDensity.compact,
-                                items: [
-                                  BlocProvider.value(
-                                    value:
-                                        BlocProvider.of<mentions.MentionsBloc>(
-                                      blocContext,
-                                    ),
-                                    child: BlocBuilder<mentions.MentionsBloc,
-                                        mentions.MentionsState>(
-                                      builder: (context, state) {
-                                        return MuffedPopupMenuItem(
-                                          title: (state.showAll)
-                                              ? 'Hide read'
-                                              : 'Show read',
-                                          onTap: () {
-                                            context
-                                                .read<mentions.MentionsBloc>()
-                                                .add(
-                                                  mentions.ShowAllToggled(),
-                                                );
-                                          },
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        );
-                  }
-                });
-
-                return NestedScrollView(
-                  headerSliverBuilder: (context, _) {
-                    return [
-                      SliverToBoxAdapter(
-                        child: SafeArea(
-                          child: TabBar(
-                            tabs: [
-                              Tab(text: 'Replies'),
-                              Tab(text: 'Mentions'),
-                            ],
-                          ),
-                        ),
+                return SetPageInfo(
+                  indexOfRelevantItem: 1,
+                  actions: [
+                    BlocProvider.value(
+                      value:
+                          BlocProvider.of<mentions.MentionsBloc>(blocContext),
+                      child: BlocBuilder<mentions.MentionsBloc,
+                          mentions.MentionsState>(
+                        builder: (context, state) {
+                          return IconButton(
+                              visualDensity: VisualDensity.compact,
+                              isSelected: state.showAll,
+                              onPressed: toggleRead,
+                              selectedIcon: Icon(Icons.remove_red_eye),
+                              icon: Icon(Icons.remove_red_eye_outlined));
+                        },
                       ),
-                    ];
-                  },
-                  body: TabBarView(
-                    children: [RepliesScreen(), MentionsScreen()],
+                    ),
+                  ],
+                  child: NestedScrollView(
+                    headerSliverBuilder: (context, _) {
+                      return [
+                        SliverToBoxAdapter(
+                          child: SafeArea(
+                            child: TabBar(
+                              tabs: [
+                                Tab(text: 'Replies'),
+                                Tab(text: 'Mentions'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ];
+                    },
+                    body: TabBarView(
+                      children: [RepliesScreen(), MentionsScreen()],
+                    ),
                   ),
                 );
               },
