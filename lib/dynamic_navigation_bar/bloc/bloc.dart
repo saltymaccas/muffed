4654cfb790ef0c19bd5dc9ca64fedc453cf4fc30
freshfieldreds <freshfieldreds@gmail.com>
2023-10-28 19:05:08 +1100

    add animation to widgets added through edit page info event

--- lib/dynamic_navigation_bar/bloc/bloc.dart
+++ lib/dynamic_navigation_bar/bloc/bloc.dart
@@ -1,9 +1,14 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'event.dart';
 part 'state.dart';
+
+const Duration _animDur = Duration(milliseconds: 500);
+const int _animInterval = 200;
+const Curve _animCurve = Curves.easeInOutCubic;
 
 /// The navigation bar that sits at the bottom of the app
 class DynamicNavigationBarBloc
@@ -34,12 +39,35 @@ class DynamicNavigationBarBloc
       );
     });
     on<EditPageActions>((event, emit) {
+      final List<Widget> animatedActions = [];
+
+      for (var i = 0; i < event.actions.length; i++) {
+        final Widget action = event.actions[i];
+        animatedActions.add(
+          action
+              .animate(autoPlay: true)
+              .slideY(
+                duration: _animDur,
+                curve: _animCurve,
+                begin: 3,
+                delay: Duration(milliseconds: _animInterval * i),
+                end: 0,
+              )
+              .fadeIn(
+                duration: _animDur,
+                begin: 0,
+                curve: _animCurve,
+                delay: Duration(milliseconds: _animInterval * i),
+              ),
+        );
+      }
+
       emit(
         state.copyWith(
           actions: Map.from(state.pageStackInfo)
             ..[event.itemIndex] = [
               ...state.pageStackInfo[event.itemIndex]!,
-              PageInfo(context: event.context, actions: event.actions),
+              PageInfo(context: event.context, actions: animatedActions),
             ],
         ),
       );
