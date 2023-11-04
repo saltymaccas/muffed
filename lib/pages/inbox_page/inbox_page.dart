2be2c301751a7a27e0e3b1f884e929afa1c21454
freshfieldreds <freshfieldreds@gmail.com>
2023-11-04 14:41:46 +1100

    make reload when account changes

--- lib/pages/inbox_page/inbox_page.dart
+++ lib/pages/inbox_page/inbox_page.dart
@@ -5,9 +5,9 @@ import 'package:muffed/global_state/bloc.dart';
 import 'package:muffed/pages/inbox_page/replies_screen/replies_screen.dart';
 import 'package:muffed/repo/server_repo.dart';
 
-import 'mentions_screen/bloc/bloc.dart' as mentions;
+import 'mentions_screen/bloc/bloc.dart' as m;
 import 'mentions_screen/mentions_screen.dart';
-import 'replies_screen/bloc/bloc.dart' as replies;
+import 'replies_screen/bloc/bloc.dart' as r;
 
 class InboxPage extends StatelessWidget {
   const InboxPage({super.key});
@@ -29,72 +29,92 @@ class InboxPage extends StatelessWidget {
           providers: [
             BlocProvider(
               create: (context) =>
-                  replies.RepliesBloc(repo: context.read<ServerRepo>())
-                    ..add(replies.Initialize()),
+                  r.RepliesBloc(repo: context.read<ServerRepo>())
+                    ..add(r.Initialize()),
             ),
             BlocProvider(
               create: (context) =>
-                  mentions.MentionsBloc(repo: context.read<ServerRepo>())
-                    ..add(mentions.Initialize()),
+                  m.MentionsBloc(repo: context.read<ServerRepo>())
+                    ..add(m.Initialize()),
             ),
           ],
           child: Builder(
             builder: (context) {
-              return DefaultTabController(
-                length: 2,
-                child: Builder(
-                  builder: (context) {
-                    final blocContext = context;
+              return BlocListener<GlobalBloc, GlobalState>(
+                listenWhen: (previous, current) {
+                  final LemmyAccountData? previousAccount =
+                      (previous.lemmySelectedAccount == -1)
+                          ? null
+                          : previous
+                              .lemmyAccounts[previous.lemmySelectedAccount];
 
-                    void toggleRead() {
-                      context
-                          .read<mentions.MentionsBloc>()
-                          .add(mentions.ShowAllToggled());
-                      context
-                          .read<replies.RepliesBloc>()
-                          .add(replies.ShowAllToggled());
-                    }
+                  final LemmyAccountData? currentAccount =
+                      (current.lemmySelectedAccount == -1)
+                          ? null
+                          : current.lemmyAccounts[current.lemmySelectedAccount];
 
-                    return SetPageInfo(
-                      indexOfRelevantItem: 1,
-                      actions: [
-                        BlocProvider.value(
-                          value: BlocProvider.of<mentions.MentionsBloc>(
-                              blocContext),
-                          child: BlocBuilder<mentions.MentionsBloc,
-                              mentions.MentionsState>(
-                            builder: (context, state) {
-                              return IconButton(
-                                  visualDensity: VisualDensity.compact,
-                                  isSelected: state.showAll,
-                                  onPressed: toggleRead,
-                                  selectedIcon: Icon(Icons.remove_red_eye),
-                                  icon: Icon(Icons.remove_red_eye_outlined));
-                            },
+                  if (previousAccount != currentAccount ||
+                      previous.lemmyDefaultHomeServer !=
+                              current.lemmyDefaultHomeServer &&
+                          current.lemmySelectedAccount == -1) {
+                    return true;
+                  }
+                  return false;
+                },
+                listener: (context, state) {
+                  context.read<r.RepliesBloc>().add(r.Initialize());
+                  context.read<m.MentionsBloc>().add(m.Initialize());
+                },
+                child: DefaultTabController(
+                  length: 2,
+                  child: Builder(
+                    builder: (context) {
+                      final blocContext = context;
+
+                      void toggleRead() {
+                        context.read<m.MentionsBloc>().add(m.ShowAllToggled());
+                        context.read<r.RepliesBloc>().add(r.ShowAllToggled());
+                      }
+
+                      return SetPageInfo(
+                        indexOfRelevantItem: 1,
+                        actions: [
+                          BlocProvider.value(
+                            value: BlocProvider.of<m.MentionsBloc>(blocContext),
+                            child: BlocBuilder<m.MentionsBloc, m.MentionsState>(
+                              builder: (context, state) {
+                                return IconButton(
+                                    visualDensity: VisualDensity.compact,
+                                    isSelected: state.showAll,
+                                    onPressed: toggleRead,
+                                    selectedIcon: Icon(Icons.remove_red_eye),
+                                    icon: Icon(Icons.remove_red_eye_outlined));
+                              },
+                            ),
                           ),
-                        ),
-                      ],
-                      child: NestedScrollView(
-                        headerSliverBuilder: (context, _) {
-                          return [
-                            SliverToBoxAdapter(
-                              child: SafeArea(
-                                child: TabBar(
-                                  tabs: [
-                                    Tab(text: 'Replies'),
-                                    Tab(text: 'Mentions'),
-                                  ],
+                        ],
+                        child: NestedScrollView(
+                          headerSliverBuilder: (context, _) {
+                            return [
+                              SliverToBoxAdapter(
+                                child: SafeArea(
+                                  child: TabBar(
+                                    tabs: [
+                                      Tab(text: 'Replies'),
+                                      Tab(text: 'Mentions'),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          ];
-                        },
-                        body: TabBarView(
-                          children: [RepliesScreen(), MentionsScreen()],
+                            ];
+                          },
+                          body: TabBarView(
+                            children: [RepliesScreen(), MentionsScreen()],
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
               );
             },
