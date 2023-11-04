73f8b2c2daaccdbbc1e1bc4ba3c5ce11a3a3055c
freshfieldreds <freshfieldreds@gmail.com>
2023-11-04 19:13:19 +1100

    add ability to go to person through link

--- lib/components/markdown_body.dart
+++ lib/components/markdown_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:go_router/go_router.dart';
+import 'package:markdown/markdown.dart' as md;
 import 'package:muffed/components/image.dart';
-import 'package:muffed/components/snackbars.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class MuffedMarkdownBody extends StatelessWidget {
   const MuffedMarkdownBody({
@@ -34,12 +34,23 @@ class MuffedMarkdownBody extends StatelessWidget {
         },
         onTapLink: (
           title,
-          link,
+          url,
           destination,
         ) async {
-          if (link != null) {
-            if (!await launchUrl(Uri.parse(link))) {
-              showErrorSnackBar(context, error: 'Failed to open link');
+          if (url != null) {
+            // CHECK IF EMAIL STYLE LINK TO USER
+            // Format: @user@lemmy.world
+            if (url.startsWith('@')) {
+              // remove the '@'
+              final username = url.substring(1);
+
+              await context.push(
+                Uri(
+                  path: '/home/person/',
+                  queryParameters: {'username': username},
+                ).toString(),
+              );
+              return;
             }
           }
         },
@@ -49,6 +60,7 @@ class MuffedMarkdownBody extends StatelessWidget {
         selectable: true,
         physics: physics,
         padding: padding,
+        inlineSyntaxes: [LemmyLinkSyntax()],
         styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
           blockquoteDecoration: BoxDecoration(
             color: Colors.transparent,
@@ -62,5 +74,27 @@ class MuffedMarkdownBody extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+// taken from: https://github.com/liftoff-app/liftoff/blob/3055896657ef05772dc5fa18c5b3ab285b93f54a/lib/widgets/markdown_text.dart#L18
+class LemmyLinkSyntax extends md.InlineSyntax {
+  // https://github.com/LemmyNet/lemmy-ui/blob/61255bf01a8d2acdbb77229838002bf8067ada70/src/shared/config.ts#L38
+  static const String _pattern =
+      r'(\/[cmu]\/|@|!)([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})';
+
+  LemmyLinkSyntax() : super(_pattern);
+
+  @override
+  bool onMatch(md.InlineParser parser, Match match) {
+    final modifier = match[1]!;
+    final name = match[2]!;
+    final url = match[3]!;
+    final anchor = md.Element.text('a', '$modifier$name@$url');
+
+    anchor.attributes['href'] = '$modifier$name@$url';
+    parser.addNode(anchor);
+
+    return true;
   }
 }
