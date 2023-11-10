d3632a6255b0ad16e88c2516e200dd7bdf93ca56
freshfieldreds <freshfieldreds@gmail.com>
2023-11-10 17:38:32 +1100

    make not sure tab bar when not logged in as when not logged in it should only show all

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:muffed/pages/home_page/HomePageView/home_page_view.dart';
 import 'package:muffed/repo/lemmy/models.dart';
+
+import '../../global_state/bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,36 +23,54 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      floatHeaderSlivers: true,
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return <Widget>[
-          SliverOverlapAbsorber(
-            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-            sliver: SliverAppBar(
-              floating: true,
-              flexibleSpace: SafeArea(
-                child: TabBar(
-                  isScrollable: true,
-                  controller: tabController,
-                  tabs: [Text('Home'), Text('Popular')],
+    return BlocBuilder<GlobalBloc, GlobalState>(
+      buildWhen: (previous, current) {
+        if (previous.isLoggedIn() != current.isLoggedIn()) {
+          return true;
+        }
+        return false;
+      },
+      builder: (context, state) {
+        if (!state.isLoggedIn()) {
+          return HomePageView(
+            mode: HomePageViewMode(listingType: LemmyListingType.all),
+          );
+        }
+
+        return NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverAppBar(
+                  floating: true,
+                  flexibleSpace: SafeArea(
+                    child: TabBar(
+                      isScrollable: true,
+                      controller: tabController,
+                      tabs: [Text('Home'), Text('Popular')],
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ];
+          },
+          body: TabBarView(
+            controller: tabController,
+            children: [
+              HomePageView(
+                mode:
+                    HomePageViewMode(listingType: LemmyListingType.subscribed),
+              ),
+              HomePageView(
+                mode: HomePageViewMode(listingType: LemmyListingType.all),
+              ),
+            ],
           ),
-        ];
+        );
       },
-      body: TabBarView(
-        controller: tabController,
-        children: [
-          HomePageView(
-            mode: HomePageViewMode(listingType: LemmyListingType.subscribed),
-          ),
-          HomePageView(
-            mode: HomePageViewMode(listingType: LemmyListingType.all),
-          )
-        ],
-      ),
     );
   }
 }
